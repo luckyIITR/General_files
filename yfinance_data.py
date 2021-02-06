f4e118093c63ffc97028f602029dbcb71fb64f35
luckyIITR <luckykushwaha56@gmail.com>
2021-02-06 22:03:36 +0530

    added file

---
+++ yfinance_data.py
@@ -0,0 +1,9 @@
+import yfinance as yf
+
+
+# get Intraday data for last 60 days
+def Daily_data(symbol,interval = "1d"):
+    symbol = symbol + ".NS"
+    data = yf.download(tickers = symbol,interval = interval,start = (datetime.now() - timedelta(days = 50)), end = datetime.now() )
+    data.index = data.index.tz_localize(None)
+    return data
